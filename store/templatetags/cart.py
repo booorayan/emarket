891cb073e4b0b230a82f82c962473c5b52172e90
brian <bryantlch@gmail.com>
2023-06-23 11:16:34 +0300

    update tags and filters

--- store/templatetags/cart.py
+++ store/templatetags/cart.py
@@ -4,12 +4,7 @@ from django.template.defaultfilters import stringfilter
 register = template.Library()
 
 
-@register.simple_tag
-def cart(products):
-    return products
-
-
-@register.filter(name='is_in-cart')
+@register.filter(name='is_in_cart')
 def is_in_cart(product, ocart):
     keys = ocart.keys()
     for iid in keys:
@@ -26,23 +21,3 @@ def cart_quantity(product, ocart):
             return ocart.get(iid)
     return 0
 
-
-@register.filter
-@stringfilter
-def currency(value):
-    return int(value)
-
-
-@register.filter
-@stringfilter
-def is_in_cart(item, ocart):
-    if item in ocart:
-        return item
-    else:
-        return None
-
-
-@register.filter
-@stringfilter
-def cart_quantity(item, ocart):
-    return ocart.count(item)
