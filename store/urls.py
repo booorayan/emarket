ea6d509d581dec0d65fa0262dab70a017567cb38
brian <bryantlch@gmail.com>
2023-06-26 16:22:18 +0300

    add signup

--- store/urls.py
+++ store/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path, include
-from .views import Index, Login, logout, store
+from .views import Index, Login, Signup, logout, store
 
 urlpatterns = [
     path('', Index.as_view(), name='homepage'),
     path('store', store, name='store'),
+    path('signup', Signup.as_view(), name='signup'),
     path('login', Login.as_view(), name='login'),
     path('logout', logout, name='logout'),
 ]
