d80639a718f2e4e8d45bd8bdb2bc6cbc44c3f9c2
brian <bryantlch@gmail.com>
2023-07-03 15:00:07 +0300

    upload media

--- store/admin.py
+++ store/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
+from .models import Products, Category, Customer, Order
+
 
 # Register your models here.
+class AdminProduct(admin.ModelAdmin):
+    list_display = ['name', 'price', 'category']
+
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ['name']
+
+
+# register app models
+admin.site.register(Products,AdminProduct)
+admin.site.register(Category)
+admin.site.register(Customer)
+admin.site.register(Order)
